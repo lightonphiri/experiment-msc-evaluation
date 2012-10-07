2182855d498e4f0dbcf52e004a4d781e5eb53f17
Lighton Phiri <lighton.phiri@gmail.com>
2012-10-07 18:59:05 +0200

    Performance Vectors Scripts - RSS performance vector script fully functional - Test case on 1k records written to output.txt and simplyctrss2.xml

--- experiments/performance/scripts/fileRSS2generator.py
+++ experiments/performance/scripts/fileRSS2generator.py
@@ -1,8 +1,9 @@
 # A module for ...
 import datetime
-import os
 import heapq
+import os
 import PyRSS2Gen
+import time
 from datetime import datetime
 from xml.dom.minidom import parse, parseString
 
@@ -17,12 +18,12 @@ def molecollector(archive):
 	except os.error as details:
 	  print "Handling error: ", details
 
-
 # function for getting a list of top 10 most recently created files based on their ctimes
 # please see http://docs.python.org/library/heapq.html
 def recentfiles(archive):
+  print "START: FEED GENERATOR[HEAPQ]: ", time.time()
   return heapq.nlargest(5, molecollector(archive))
-
+  print "END: FEED GENERATOR[HEAPQ]: ", time.time()
 
 # function to read xml document and pull out relevant information for feeds
 # title -> dc:title
@@ -31,19 +32,24 @@ def recentfiles(archive):
 # pubDate -> creation date from recentfiles() function
 # guid -> unique archive path
 def RSS2format(inputfile):
+  print "START: FEED GENERATOR[ITEM OBJECT CREATOR]: ", time.time()
   xmldocument = parse(inputfile)
   feed_title = ""
   try:
     feed_title = xmldocument.getElementsByTagName('dc:title')[0].firstChild.data
   except IndexError as details:
-    feed_title = "dc:title missing..."
+    print "Handling IndexError: ", details
+    feed_title = "Handling IndexError..."
+  except AttributeError as details:
+    print "Handling AttributeError: ", details
+    feed_title = "Handling AttributeError..."
   # only get first 100 characters.. RSS
   feed_description = ""
   try:
     feed_description = xmldocument.getElementsByTagName('dc:description')[0].firstChild.data[:100]
   except IndexError as details:
-    print "Handling Error: ", details
-    feed_description = "dc:description missing..."
+    print "Handling IndexError: ", details
+    feed_description = "Handling IndexError"
   feed_link = xmldocument.getElementsByTagName('identifier')[0].firstChild.data # get header identifier for link value
   feed_pubDate = xmldocument.getElementsByTagName('datestamp')[0].firstChild.data # get header datestamp for pubDate value
   feed_guid = xmldocument.getElementsByTagName('identifier')[0].firstChild.data # get header identifier for guid value
@@ -55,9 +61,11 @@ def RSS2format(inputfile):
     guid = feed_guid,
     pubDate = datetime.strptime(feed_pubDate.replace("T", " ").replace("Z", ""), '%Y-%m-%d %H:%M:%S')
   )
+  print "END: FEED GENERATOR[ITEM OBJECT CREATOR]: ", time.time()
 
 # function to write list results from recentfiles function
 def writeRSS2file(inputitems):
+  print "START: FEED GENERATOR[WRITING]: ", time.time()
   feed_items = [] # define empty list
   for item in inputitems:
     print "appending... ", item[1]
@@ -71,3 +79,4 @@ def writeRSS2file(inputitems):
   )
   print feed_items
   rss.write_xml(open("simplyctrss2.xml", "w"))
+  print "END: FEED GENERATOR[WRITING]: ", time.time()
--- experiments/performance/scripts/oaipmh2simplyct.py
+++ experiments/performance/scripts/oaipmh2simplyct.py
@@ -1,10 +1,12 @@
 # A module that creates a simplyct archive using an oaipmh response output as input
-
 import os
+import time
 import xml
 from xml.dom.minidom import parse, parseString
+import fileRSS2generator
 
 def oaipmh2simplyctparser(inputfile):
+  print "START: PREPROCESSING[PARSING]: ", time.time()
   oaipmhdom = parse(inputfile) # parse xml file
   for record in oaipmhdom.getElementsByTagName('record'):
     # get dc:identifier for filename
@@ -25,9 +27,14 @@ def oaipmh2simplyctparser(inputfile):
     # call function for writing xmldocument
     container = '../data/archive/' + container
     identifier = identifier + '.metadata'
+    print "END: PREPROCESSING[PARSING]: ", time.time()
     simplyctwriter(xmldocument.toxml(), container, identifier)
+    # start calling functions in fileRSS2generator module
+    archivedir = '/home/phiri/Projects/masters/msc-evaluation/experiments/performance/data/archive'
+    fileRSS2generator.writeRSS2file(fileRSS2generator.recentfiles(archivedir))
     
 def simplyctwriter(xmldata, directory, filename):
+  print "START: PREPROCESSING[WRITING]: ", time.time()
   # handle potentially malformed xml content
   xmldata = str(xmldata).encode('ascii', 'ignore')
   # construct path to write output to
@@ -36,3 +43,4 @@ def simplyctwriter(xmldata, directory, filename):
   xmlwriter = open(os.path.join(directory, filename), mode='w')
   xmlwriter.write(xmldata)
   xmlwriter.close()
+  print "END: PREPROCESSING[WRITING]: ", time.time()
