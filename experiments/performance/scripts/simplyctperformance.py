8af90d67f13f0aa22e950d42e0fb1f9cc99fed08
Lighton Phiri <lighton.phiri@gmail.com>
2012-12-08 18:06:54 +0200

    Performance Experiments - Added experiment workload model - Added partial script for spawning workload

---
+++ experiments/performance/scripts/simplyctperformance.py
@@ -0,0 +1,30 @@
+"""This module provides workload-related functions for performing
+performance evaluation experiments.
+
+"""
+
+__name__ = "simplyctperformance"
+__version__ = "1.0.0"
+__author__ = "Lighton Phiri"
+__email__ = "lighton.phiri@gmail.com"
+
+import os
+import shutil
+
+def spawnworkload(dataset, destination):
+    workloads = (('w1',100), ('w2',200), ('w3',400), ('w4',800), ('w5',1600), ('w6',3200), ('w7',6400), ('w8',12800), ('w9',25600), ('w10',51200), ('w11',102400), ('w12',204800), ('w13',409600), ('w14',819200))
+    for workload in workloads:
+        workloadname = workload[0]
+        workloadlimit = workload[1]
+        workloadvalue = 0
+        for root, dirs, files in os.walk(dataset):
+            for filename in files:
+                if filename.endswith('.metadata'):
+                    directory = os.path.dirname(os.path.relpath(os.path.abspath(os.path.join(root, file)), dataset))
+                    workloadfile = os.path.relpath(os.path.abspath(os.path.join(root, file)), dataset)
+                    if not os.path.exists(directory):
+                        os.mkdirs(directory)
+                    shutil.copy2(workloadfile, destination)
+                    workloadvalue += 1
+        if workloadvalue > workloadlimit:
+            continue
